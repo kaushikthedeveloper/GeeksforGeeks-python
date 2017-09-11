b25550c28cb187545be8fa6a3a1e917d7dc2a1eb
kaushikthedeveloper <kaushikthedeveloper@gmail.com>
2017-09-11 19:52:10 +0530

    Add CodeFormat file

---
+++ CodeFormat.py
@@ -0,0 +1,28 @@
+#<link>
+
+class <data_struct>:
+    ...
+
+def <primary_func>():
+    ...
+
+def <helper_func>():
+    ...
+
+#main
+if __name__=="__main__":
+    ...
+
+"""
+Input Explanation :
+ - 
+
+Input :
+
+Output :
+
+"""
+
+'''
+<GeeksforGeeks page content>
+'''
